b04f6a2a0e5f6617c9693eca6429800faa91baf3
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2024-10-18 20:03:33 +0300

    add free memory clip

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from .free_memory_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
 
-print("ComfyUI-FreeMemory nodes loaded: Image, Latent, and Model versions available.")
+print("ComfyUI-FreeMemory nodes loaded: Image, Latent, Model, and CLIP versions available.")
