2b58f9327edc0f6b49c0fba2d127af3f0a33dd76
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2025-01-30 07:27:06 +0200

    Add free memory string

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from .free_memory_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
 
-print("ComfyUI-FreeMemory nodes loaded: Image, Latent, Model, and CLIP versions available.")
+print("ComfyUI-FreeMemory nodes loaded: Image, Latent, Model, CLIP, and String versions available.")
